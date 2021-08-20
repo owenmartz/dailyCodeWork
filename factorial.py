234ecb687408694ba84afb4cf37674aff80f28df
Martin Owen <martinowen555@gmail.com>
2021-08-20 02:44:17 +0000

    Added different factorial functions

--- factorial.py
+++ factorial.py
@@ -13,8 +13,40 @@
 
 
 
-def last_factorial_digit(n):
-  pass
+#regular factorial code
+#using a while loop
+def fact(n):
+  result = 1
+  while n > 1:
+    result = n * result
+    n -= 1
+  return result
 
+#using a for loop
+
+def facto(n):
+  result = 1
+  if n >= 1:
+    for i in range(1, n+1):
+      result = result * i
+    return result
+  else:
+    print("N has to be a positive number!")
+
+#using recursive solution, we need a base case and a recursive case
+
+def factoriall(n):
+  if n <= 1:
+    return 1
+  else:
+    return n * factoriall(n-1)
+  
+
+
+
+print(factoriall(1))
+print(factoriall(2))
+print(factoriall(3))
+print(factoriall(4))
 
 
